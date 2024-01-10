d4ba51901aebf88a6f3e4c4d7e339b9e12f3efdb
Daniel Luis <danniellcontato@gmail.com>
2024-01-10 12:29:08 -0300

    Update do ponteiro segundo

--- js/script.js
+++ js/script.js
@@ -10,7 +10,10 @@ function atualiza(){
     const minuto = date.getMinutes();
     const hora = date.getHours();
 
-    ponteiroSegundo.style.transform = `translate(0, -50%) rotate(${segundo * 6}deg)`;
+    /*ponteiroSegundo.style.transform = `translate(0, -50%) rotate(${segundo * 6}deg)`;
+    ponteirominuto.style.transform = `translate(0, -50%) rotate(${minuto * 6}deg)`;
+    ponteirohora.style.transform = `translate(0, -50%) rotate(${hora * 30}deg)`;*/
+    ponteiroSegundo.style.transform = `rotate(${segundo * 6}deg)`;
     ponteirominuto.style.transform = `translate(0, -50%) rotate(${minuto * 6}deg)`;
     ponteirohora.style.transform = `translate(0, -50%) rotate(${hora * 30}deg)`;
 };
